c26400d52ee8cbfdb8e78b7652e7d258d0e8631a
“VISHNU <vishnu.k@copass.in>
2023-12-13 12:32:45 +0530

    slided section added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
+let bookNowBtn = document.getElementById('book-now-btn')
+
 document.getElementById('bookConsultationForm').addEventListener('submit', bookConsultation);
+
+
 
 function bookConsultation(event){
 
     alert('booking Consultation')
 }
+
+function bookNow(){
+    alert('model show')
+}
+
+
+bookNowBtn.addEventListener('click',bookNow)
