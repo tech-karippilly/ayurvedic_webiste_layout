041dc4fd257976a1c702fbab3067a313745d460f
“tech-karippilly” <vishnukarippilly@gmail.com>
2023-12-21 23:49:52 +0530

    ui update form section

--- js/index.js
+++ js/index.js
@@ -1,12 +1,38 @@
 let bookNowBtn = document.getElementById('book-now-btn')
 
-document.getElementById('bookConsultationForm').addEventListener('submit', bookConsultation);
+// let bookConsultationBtn = document.getElementById('bookConsultationForm')
+
+// console.log(bookConsultationBtn);
+// // bookConsultationBtn.addEventListener('submit', bookConsultation);
 
 
 
-function bookConsultation(event){
-    console.log('booking Consultation');
-    // alert('booking Consultation')
+// function bookConsultation(event){
+//     console.log('booking Consultation');
+//     // alert('booking Consultation')
+// }
+
+
+let modelButton = document.getElementById('model-button')
+
+modelButton.addEventListener('click',scrollToSection)
+
+function handleScroll(event) {
+    let scrollYValue = window.scrollY;
+    console.log(scrollYValue);
+
+}
+
+function scrollToSection(event){
+
+    window.scrollTo(0,4017.17)
+    // event.preventDefault();
+    // let section = document.getElementById('form-section');
+    // if (section) {
+    //     section.scrollIntoView({ behavior: 'smooth' });
+    //   }
 }
 
 
+
+window.addEventListener('scroll', handleScroll)
