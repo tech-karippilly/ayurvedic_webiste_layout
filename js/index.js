dd7bc5203a1897d198d95ebaad73bbccf6494272
“tech-karippilly” <vishnukarippilly@gmail.com>
2023-12-23 22:24:42 +0530

    arrow added

--- js/index.js
+++ js/index.js
@@ -14,6 +14,7 @@ let bookNowBtn = document.getElementById('book-now-btn')
 
 
 let modelButton = document.getElementById('model-button')
+let downArrowContainer = document.getElementById('down-arrow')
 
 modelButton.addEventListener('click',scrollToSection)
 
@@ -21,17 +22,30 @@ function handleScroll(event) {
     let scrollYValue = window.scrollY;
     console.log(scrollYValue);
 
+    if (scrollYValue > 10){
+        downArrowContainer.style="display:none"
+    }else{
+        downArrowContainer.style="display:flex"
+    }
+
 }
 
-function scrollToSection(event){
 
-    window.scrollTo(0,4017.17)
+
+function scrollToSection(event){
+    
     // event.preventDefault();
     // let section = document.getElementById('form-section');
     // if (section) {
     //     section.scrollIntoView({ behavior: 'smooth' });
     //   }
 }
+function downArrowClick(){
+    window.scrollBy(0,500)
+   
+}
+
+downArrowContainer.addEventListener('click',downArrowClick)
 
 
 
